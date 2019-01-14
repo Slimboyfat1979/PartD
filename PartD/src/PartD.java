3099a56231adeec644d8e2766b0fe8949314eb91
Slimboyfat1979 <DavidBarnett@myopenpolytechnic.ac.nz>
2019-01-15 01:01:10 +1300

    Third line of program

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -4,6 +4,8 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("My name is David");
 		System.out.println("My Favourite colour is Blue");
+		System.out.println("My favourite mode of transportation is Car");
+		
 
 	}
 
