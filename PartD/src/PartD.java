48e0aaa46c0f8e566e8d3ea2f53856052d4a369a
Slimboyfat1979 <DavidBarnett@myopenpolytechnic.ac.nz>
2019-01-15 01:02:04 +1300

    Fourth line of code

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -5,6 +5,7 @@ public class PartD {
 		System.out.println("My name is David");
 		System.out.println("My Favourite colour is Blue");
 		System.out.println("My favourite mode of transportation is Car");
+		System.out.println("The best day of the week is Friday");
 		
 
 	}
